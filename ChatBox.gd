14e73e487214666f60a5c6600e0316828e892194
craig <40326682+craig@users.noreply.github.com>
2020-12-02 17:55:00 -0800

    Added command manager, /invite
    Command Manager node added to Chat Box, can process one command "/invite"
    
    Also tidied up how ChatBox.gd processes different types of messages

--- ChatBox.gd
+++ ChatBox.gd
@@ -1,8 +1,9 @@
 extends Control
 
-onready var chatLog = get_node("MarginContainer/VBoxContainer/RichTextLabel")
-onready var inputLabel = get_node("MarginContainer/VBoxContainer/ColorRect/HBoxContainer/Label")
-onready var inputField = get_node("MarginContainer/VBoxContainer/ColorRect/HBoxContainer/LineEdit")
+onready var chatLog = $MarginContainer/VBoxContainer/RichTextLabel
+onready var inputLabel = $MarginContainer/VBoxContainer/ColorRect/HBoxContainer/Label
+onready var inputField = $MarginContainer/VBoxContainer/ColorRect/HBoxContainer/LineEdit
+onready var cmdManager = $CommandManager
 
 var has_focus = false
 
@@ -19,6 +20,8 @@ func _ready():
 	inputField.connect("text_entered", self, "on_text_entered")
 	inputField.connect("focus_entered", self, "on_focus_entered")
 	inputField.connect("focus_exited", self, "on_focus_released")
+	cmdManager.connect("command_msg_logged", self, "on_command_msg_logged")
+	cmdManager.connect("error_msg_logged", self, "on_error_msg_logged")
 	
 	change_group(0)
 
@@ -38,21 +41,44 @@ func change_group(value):
 	inputLabel.text = '[' + groups[group_index]['name'] + ']'
 	inputLabel.set('custom_colors/font_color', Color(groups[group_index]['color']))
 
-func add_message(username, text, group = 0):
+func add_message(text, color):
 	# Add leading linebreak only if chat box is empty 
 	if chatLog.bbcode_text != '':
 		chatLog.bbcode_text += '\n'
-	chatLog.bbcode_text += '[color=' + groups[group]['color'] + ']'
-	chatLog.bbcode_text += '[' + username + ']: '
+	chatLog.bbcode_text += '[color=' + color + ']'
 	chatLog.bbcode_text += text
 	chatLog.bbcode_text += '[/color]'
+
+func add_chat_message(username, text, group = 0):
+	var color = groups[group]['color']
+	var final_text = '[' + username + ']: '
+	final_text += text
+	add_message(final_text, color)
+
+func add_system_log(text):
+	var final_text = text
+	add_message(final_text, "white")
+
+func add_error_log(text):
+	var final_text = "[ERROR]: "
+	final_text += text
+	add_message(final_text, "red")
 
 # Not sure how to make it so you can leave text in the inputfield, release focus
 # then grab focus again with Enter without sending the leftover text
 func on_text_entered(new_text):
 	if new_text != '':
-		add_message(user_name, new_text, group_index)
+		if new_text[0] == '/':
+			cmdManager.on_command_entered(new_text)
+		else:
+			add_chat_message(user_name, new_text, group_index)
 		inputField.text = ''
+
+func on_command_msg_logged(text):
+	add_system_log(text)
+
+func on_error_msg_logged(text):
+	add_error_log(text)
 
 func on_focus_entered():
 	has_focus = true
---
+++ Scenes/CommandManager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+signal command_msg_logged(text)
+signal error_msg_logged(text)
+signal cmd_invite(target)
+
+func on_command_entered(text : String):
+	# there's probably a way to make CommandChecking less manual
+	if text.substr(1, 7) == 'invite ' or text.substr(1, 4) == 'inv ':
+		var target = text.split(' ')[1]
+		# if target exists and is not in a party: emit_signal("cmd_invite", target)
+		var msg = 'You invited ' + target + ' to join your party.'
+		emit_signal('command_msg_logged', msg)
+	else:
+		var bad_cmd = text.split(' ')[0]
+		var msg = 'Command "' + bad_cmd + '" not recognized. Type /cmds for a complete list of available commands.'
+		emit_signal('error_msg_logged', msg)
+#/invite
